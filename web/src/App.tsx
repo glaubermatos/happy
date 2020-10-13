fe22be81c0a33db75c5755da1d51f58747af474e
glaubermatos <glaub.oliveira@hotmail.com>
2020-10-13 19:42:09 -0300

    Landing page criada

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import { FiArrowRight } from 'react-icons/fi'
+
+import './styles/global.css'
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello word!</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo happy"/>
+        <main>
+          <h1>
+            Leve felicidade para o mundo
+          </h1>
+          <p>
+            Visite orfanatos e mude o dia
+            de muitas crianças.
+          </p>
+        </main>
+        <div className="location">
+          <strong>Manoel Vitorino</strong>
+          <span>Bahia</span>
+        </div>
+
+        <a href="#" className="enter-app">
+          <FiArrowRight size="24" color="rgba(0, 0, 0, .6)" />
+        </a>
+      </div>
     </div>
   );
 }
