e294c6fa61b0aa2819df36f6db24d9314cbb1b12
glaubermatos <glaub.oliveira@hotmail.com>
2020-10-14 03:27:09 -0300

    Adicionando mapa na página de listagem de orfanatos

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
 
 import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css'
+import 'leaflet/dist/leaflet.css'
 
 function OrphanagesMap() {
     return (
@@ -23,7 +25,13 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+              center={[-14.1465684,-40.2442925]}
+              zoom={15}
+              style={{width: '100%', height: '100%'}}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
 
             <Link to="/" className="create-orphanage">
                 <FiPlus size="32" color="#fff" />
