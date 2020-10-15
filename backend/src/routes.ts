d87e66c8779aa11f6a8638a0f9906ef5e1bfaee3
glaubermatos <glaub.oliveira@hotmail.com>
2020-10-15 00:30:20 -0300

    Adicionando rota de listagem de orfanatos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,9 @@ import OrphanagesController from './controllers/OrphanagesController'
 
 const routes = Router();
 
+//  métodos comuns -> index, show, create, update e delete
+
+routes.get('/orphanages', OrphanagesController.index);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
