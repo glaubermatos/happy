2ac05a1fb9f4a678309f5c9a5034a0c327176695
glaubermatos <glaub.oliveira@hotmail.com>
2020-10-14 23:41:15 -0300

    configurando database, connection, orm e migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,38 @@
 import express from 'express'
+import { getRepository } from 'typeorm';
+
+import './database/connection'
+import Orphanage from './models/Orphanages';
 
 const app = express();
 
-app.get('/users', function(request, response){
-    const users = [
-        {name: 'João', age: 32},
-        {name: 'José', age: 31},
-        {name: 'Pedro', age: 42},
-    ]
-    response.json(users);
+app.use(express.json());
+
+app.post('/orphanages', async function(request, response){
+    const { 
+        name, 
+        latitude, 
+        longitude, 
+        about, 
+        instructions, 
+        opening_hours, 
+        open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanage = orphanagesRepository.create({
+        name, 
+        latitude, 
+        longitude, 
+        about, 
+        instructions, 
+        opening_hours, 
+        open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+    
+    response.status(201).json(orphanage);
 });
 
 app.listen(3333);
